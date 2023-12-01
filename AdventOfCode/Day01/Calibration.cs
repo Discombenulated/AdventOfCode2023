81a3bd11cf402b00a4633fd341ee5bbc4af60fae
Ben Noble <ben@benshouse.net>
2023-12-01 09:54:22 +0000

    Able to specify line type

--- AdventOfCode/Day01/Calibration.cs
+++ AdventOfCode/Day01/Calibration.cs
@@ -4,15 +4,22 @@ namespace AdventOfCode.Day01;
 
 public class Calibration
 {
+    public delegate CalibrationLine CreateLineDelegate(string line);
     private readonly string[] lines;
-    public Calibration(string[] lines){
+    private readonly CreateLineDelegate lineFactory;
+
+    public Calibration(string[]lines) : this(lines, delegate(string line) {return new CalibrationLine(line);})
+    {}
+
+    public Calibration(string[] lines, CreateLineDelegate lineFactory){
         this.lines = lines;
+        this.lineFactory = lineFactory;
     }
 
     public int Sum(){
         int sum = 0;
         foreach (var line in lines){
-            sum += new CalibrationLine(line).Value();
+            sum += this.lineFactory(line).Value();
         }
         return sum;
     }
